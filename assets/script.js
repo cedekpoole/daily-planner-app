0234baa5d4ba4a23fe78dd7a0e6b8100c7765094
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-23 11:42:40 +0000

    add additional time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
-
-
 // Display current date at the top of the page
-const day = $("#currentDay");
+const dayDisplay = $("#currentDay");
 
-const currentD = moment().format('Do MMMM YYYY');
+const currentD = moment().format("Do MMMM YYYY");
 
-day.text(currentD);
+dayDisplay.text(currentD);
+
+$(".row").each(function () {
+  // change the colour of textarea when time changes
+  var timeSection = $(this).attr("id");
+  var hour = moment().format("HH");
+
+  if (timeSection === hour) {
+    $(this).addClass("present");
+  } else if (timeSection < hour) {
+    $(this).addClass("future");
+    $(this).removeClass("present");
+  } else {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+  }
+  // show time in first column of each row
+  var amPm = $(this).children(".hour").children().attr("id");
+  $(this).children(".hour").children().text(amPm);
+});
+
