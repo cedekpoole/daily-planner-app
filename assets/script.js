abd00268fa858758023cae9d4f9e98fea333f6f8
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-23 14:31:33 +0000

    store data in localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-// Display current date at the top of the page
+// Display current date (day, month and year) at the top of the page
 const dayDisplay = $("#currentDay");
 
 const currentD = moment().format("Do MMMM YYYY");
 
 dayDisplay.text(currentD);
 
+// use jQuery to loop through each .row element to compare current hour to time block
 $(".row").each(function () {
   // change the colour of textarea when time changes
   var timeSection = $(this).attr("id");
@@ -24,3 +25,31 @@ $(".row").each(function () {
   $(this).children(".hour").children().text(amPm);
 });
 
+$(".save-button").on("click", (e) => {
+  // store user's input associated with particular save button into a variable
+  var storeItem = $(e.currentTarget).parent().children().eq(1).val();
+  // store clicked box (the time) into a variable
+  var time = $(e.currentTarget).parent().attr("id");
+  // set item into local storage (as a key: value pair)
+  localStorage.setItem(time, storeItem);
+});
+
+// Retrieve previous user inputs from local storage and display them 
+$("#09 .time-block").val(localStorage.getItem("09"));
+$("#10 .time-block").val(localStorage.getItem("10"));
+$("#11 .time-block").val(localStorage.getItem("11"));
+$("#12 .time-block").val(localStorage.getItem("12"));
+$("#13 .time-block").val(localStorage.getItem("13"));
+$("#14 .time-block").val(localStorage.getItem("14"));
+$("#15 .time-block").val(localStorage.getItem("15"));
+$("#16 .time-block").val(localStorage.getItem("16"));
+$("#17 .time-block").val(localStorage.getItem("17"));
+
+// create a function that clears user data from local storage
+var clear = () => {
+    localStorage.clear();
+    $("textarea").val("")
+}
+
+// when 'clear data' button is clicked, clear function is executed
+$("#clear-button").on("click", clear);
